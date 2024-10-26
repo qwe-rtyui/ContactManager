6be99b647a02dd6f9af9a777e5e1fca4e4e172f6
qwe-rtyui <kesleyjuan.dev@gmail.com>
2024-10-25 21:30:37 -0300

    primeiro commit ContactManager

---
+++ Services/ContactManagerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.Services
+{
+    public class ContactManagerController
+    {
+        
+    }
+
+    public class ContactSettings
+    {
+        public string Image;
+        public string Name;
+        public string Phone;
+        public string Email;
+        public string Description;
+       
+    }
+}
